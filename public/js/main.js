2711ad09402050354ae46b7b347c75923a8b0c65
Aditya <addy.7kad@gmail.com>
2019-08-17 19:59:56 +0530

    animations for homepage added

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,104 @@ $(document).ready(function() {
 	t1.fromTo(logo, 1.5 ,{x: "-100%"},{x:"0%",ease: Power2.easeInOut})
 	.fromTo(nav_list, 1.5, {x: "100%"},{x:"0%",ease: Power2.easeInOut},"-=1.5"); 
 
+	var wrapper = $('.animation-holder');
+	var fadeInLeft = $('.animation-holder-fade');
+	var fadeIn= $('.animation-holder-fadeIn');
+	// for (i=0; i < wrapper.length; i++) {
+	// 	// console.log(typeof(wrapper));
+	// 	var counted = wrapper[i];
+	// 	console.log(counted);
+	// 	console.log(wrapper);
+	// 	wrapper.waypoint(function() {
+	// 		wrapper.addClass('bounceIn animated');
+	// 	},{offset:'85%'});
+		
+	// }
+	
+
+	// fadeInLeft.waypoint(function() {
+	// 	fadeInLeft.addClass('fadeInLeftBig animated');
+	// },{offset:'85%'});
+	
+	// fadeIn.waypoint(function() {
+	// 	fadeIn.addClass('fadeIn fade-animated');
+	// },{offset:'85%'});
+
+	$.each(['animation-holder'], function(i, classname) {
+		var $elements = $('.' + classname)
+		$elements.each(function() {
+		  new Waypoint({
+			element: this,
+			handler: function(direction) {
+			  var previousWaypoint = this.previous()
+			  var nextWaypoint = this.next()
+	  
+			  $elements.removeClass('np-previous np-current np-next')
+			  $(this.element).addClass('np-current bounceIn animated')
+			  if (previousWaypoint) {
+				$(previousWaypoint.element).addClass('np-previous')
+			  }
+			  if (nextWaypoint) {
+				$(nextWaypoint.element).addClass('np-next')
+			  }
+			},
+			offset: '70%',
+			group: classname
+		  })
+		})
+	});
+
+	$.each(['animation-holder-fade'], function(i, classname) {
+		var $elements = $('.' + classname)
+		$elements.each(function() {
+		  new Waypoint({
+			element: this,
+			handler: function(direction) {
+			  var previousWaypoint = this.previous()
+			  var nextWaypoint = this.next()
+	  
+			  $elements.removeClass('np-previous np-current np-next')
+			  $(this.element).addClass('np-current fadeInLeftBig animated')
+			  if (previousWaypoint) {
+				$(previousWaypoint.element).addClass('np-previous')
+			  }
+			  if (nextWaypoint) {
+				$(nextWaypoint.element).addClass('np-next')
+			  }
+			},
+			offset: '75%',
+			group: classname
+		  })
+		})
+	});
+
+	$.each(['animation-holder-fadeIn'], function(i, classname) {
+		var $elements = $('.' + classname)
+		$elements.each(function() {
+		  new Waypoint({
+			element: this,
+			handler: function(direction) {
+			  var previousWaypoint = this.previous()
+			  var nextWaypoint = this.next()
+	  
+			  $elements.removeClass('np-previous np-current np-next')
+			  $(this.element).addClass('np-current fadeIn fade-animated')
+			  if (previousWaypoint) {
+				$(previousWaypoint.element).addClass('np-previous')
+			  }
+			  if (nextWaypoint) {
+				$(nextWaypoint.element).addClass('np-next')
+			  }
+			},
+			offset: '75%',
+			group: classname
+		  })
+		})
+	});
+
+
+
+
 	//Light Gallery
 
 	$('.gallery-wrapper').lightGallery();
@@ -20,7 +118,7 @@ $(document).ready(function() {
 		method: 'GET',
 		url: '/demo',
 		success: function(data) {
-			console.log(data);
+			// console.log(data);
 		},
 		error: function() {
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,8 +17,10 @@
 
 <section class="services-section">
 	<div class="container">
-		<h2 class="services-heading">SALON AND BARBAR SERVICE</h2>
-		<p class="services-desc">WE HELP YOU LOOK GREAT</p>
+		<div class="animation-holder">
+			<h2 class="services-heading">SALON AND BARBAR SERVICE</h2>
+			<p class="services-desc">WE HELP YOU LOOK GREAT</p>
+		</div>
 		<div class="card-wrapper">
 			<div class="row">
 			<!-- @foreach($services as $service)
@@ -30,22 +32,28 @@
 				</div>
 			@endforeach -->
 				<div class="col-sm-4">
-					<img src="{{asset('images/home/service-icon-1.png')}}" alt="service-icon-1" class="img-fluid">
-					<h4 class="service-head">MUSTACHE TRIM</h4>
-					<p class="service-description">Free Responsive HTML5 CSS3 Website Template for hair salon and beauty salon.</p>
-					<p class="service-price">$45</p>
+					<div class="animation-holder">
+						<img src="{{asset('images/home/service-icon-1.png')}}" alt="service-icon-1" class="img-fluid">
+						<h4 class="service-head">MUSTACHE TRIM</h4>
+						<p class="service-description">Free Responsive HTML5 CSS3 Website Template for hair salon and beauty salon.</p>
+						<p class="service-price">$45</p>
+					</div>
 				</div>
 				<div class="col-sm-4">
-					<img src="{{asset('images/home/service-icon-2.png')}}" alt="service-icon-2" class="img-fluid">
-					<h4 class="service-head">MUSTACHE TRIM</h4>
-					<p class="service-description">Free Responsive HTML5 CSS3 Website Template for hair salon and beauty salon.</p>
-					<p class="service-price">$45</p>
+					<div class="animation-holder">
+						<img src="{{asset('images/home/service-icon-2.png')}}" alt="service-icon-2" class="img-fluid">
+						<h4 class="service-head">MUSTACHE TRIM</h4>
+						<p class="service-description">Free Responsive HTML5 CSS3 Website Template for hair salon and beauty salon.</p>
+						<p class="service-price">$45</p>
+					</div>
 				</div>
 				<div class="col-sm-4">
-					<img src="{{asset('images/home/service-icon-3.png')}}" alt="service-icon-3" class="img-fluid">
-					<h4 class="service-head">BEARD TRIM</h4>
-					<p class="service-description">Responsive website templates free download html with css.</p>
-					<p class="service-price">$45</p>
+					<div class="animation-holder">
+						<img src="{{asset('images/home/service-icon-3.png')}}" alt="service-icon-3" class="img-fluid">
+						<h4 class="service-head">BEARD TRIM</h4>
+						<p class="service-description">Responsive website templates free download html with css.</p>
+						<p class="service-price">$45</p>
+					</div>
 				</div>
 			</div>
 			<div class="service-btn-wrapper">
@@ -60,10 +68,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6">
-				<img src="{{asset('images/home/about-img.jpg')}}" alt="about-img" class="img-fluid">
+				<div class="animation-holder-fadeIn">	
+					<img src="{{asset('images/home/about-img.jpg')}}" alt="about-img" class="img-fluid">
+				</div>
 			</div>
 			<div class="col-lg-6">
-				<div class="about-description">
+				<div class="about-description animation-holder-fadeIn">
 					<h2>MEN’S SALON WEBSITE TEMPLATES</h2>
 					<p>BEST EXPERIENCE EVER</p>
 					<p>Free Hair Salon Website Templates for your hair salon shop or business. <a href=""> Free Website Template Download It now!</a></p>
@@ -78,24 +88,32 @@
 
 <section class="testimonials-section">
 	<div class="container">
-		<h2 class="test-head">TESTIMONIALS</h2>
-		<p class="test-description">WHAT HAPPY CLIENT SAY</p>
+		<div class="animation-holder">
+			<h2 class="test-head">TESTIMONIALS</h2>
+			<p class="test-description">WHAT HAPPY CLIENT SAY</p>
+		</div>
 		<div class="card-wrapper">
 			<div class="row">
 				<div class="col-sm-4">
-					<p class="test-content">“Free Beauty Website Templates that help me a lot to build easy and fast my hair shop website in 2 days”</p>
-					<p class="author-name">REBA TRUONG</p>
-					<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					<div class="animation-holder">
+						<p class="test-content">“Free Beauty Website Templates that help me a lot to build easy and fast my hair shop website in 2 days”</p>
+						<p class="author-name">REBA TRUONG</p>
+						<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					</div>
 				</div>
 				<div class="col-sm-4">
-					<p class="test-content">“Free bootstrap responsive website templates 2017 its best ever i found for my hair salon”</p>
-					<p class="author-name">THOMAS WARREN</p>
-					<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					<div class="animation-holder">
+						<p class="test-content">“Free bootstrap responsive website templates 2017 its best ever i found for my hair salon”</p>
+						<p class="author-name">THOMAS WARREN</p>
+						<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					</div>	
 				</div>
 				<div class="col-sm-4">
-					<p class="test-content">“Best Free HTML CSS Website Templates for salon and hair cutting business. All features are clean designed”</p>
-					<p class="author-name">CARIE WILLIS</p>
-					<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					<div class="animation-holder">
+						<p class="test-content">“Best Free HTML CSS Website Templates for salon and hair cutting business. All features are clean designed”</p>
+						<p class="author-name">CARIE WILLIS</p>
+						<p class="author-description">34 YEAR &nbsp;&nbsp; CUSTOMER</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -115,7 +133,9 @@
 </section>
 <section class="news-section">
 	<div class="container">
-		<h2 class="news-head">LATEST NEWS & ARTICLE</h2>
+		<div class="animation-holder">
+			<h2 class="news-head">LATEST NEWS & ARTICLE</h2>
+		</div>
 		<div class="row news-row">
 			<div class="col-lg-6">
 				<img src="{{asset('images/home/post-img.jpg')}}" alt="" class="img-fluid">
